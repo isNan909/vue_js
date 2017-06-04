47e813277db33d2b5a2ff6ceb50072bea2e34bfc
Ishan Manandhar <ishan.manandhar@wolfmatrix.com>
2017-06-04 10:12:55 +0545

    Data and Methods

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 new Vue({
     el: '#app',
     data: {
-        name: 'Ishan'
+        name: 'Ishan',
+        designation:'designer'
+    },
+    methods: {
+        greet: function(time){
+            return 'Good ' + time + ', ' + this.name;
+        }
     }
 });
